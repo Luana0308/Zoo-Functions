e26165a416f13dc7a1d903723f3a3f590cf1eda0
Luana <luanaff-2008@hotmail.com>
2022-01-06 19:26:00 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const { employees } = data;
+  const objectEmployees = employees.filter((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName)[0];
+  if (employeeName === undefined) {
+    return {};
+  }
+  return objectEmployees;
 }
 
 module.exports = getEmployeeByName;
