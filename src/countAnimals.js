2814488f1c41013128f65e9729e5bd4b2549bf83
Luana <luanaff-2008@hotmail.com>
2022-01-07 18:22:33 -0300

    desafio 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const allAnimals = species.reduce((acc, valorAnimal) =>
+    ({ ...acc, [valorAnimal.name]: valorAnimal.residents.length }), {});
+  if (animal === undefined) {
+    return allAnimals;
+  }
+  const { specie } = animal;
+  const sexAnimal = species.find((animalselected) => animalselected.name === specie);
+  const aminalsexSelected = sexAnimal.residents.reduce((acc, valor) => {
+    if (valor.sex === animal.sex) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+  if (animal.sex) return aminalsexSelected;
+  return allAnimals[specie];
 }
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
+
+// ajuda da Aluna Rachel - e do grupo de estudos
