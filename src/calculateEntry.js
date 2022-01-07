0cdb9f09c9fafb2341759d80f920cc8b2c40ace6
Luana <luanaff-2008@hotmail.com>
2022-01-07 20:14:23 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,19 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const childs = entrants.filter((child) => child.age < 18);
+  const adults = entrants.filter((adult) => adult.age >= 18 && adult.age < 50);
+  const seniors = entrants.filter((senior) => senior.age >= 50);
+  return { adult: adults.length, child: childs.length, senior: seniors.length };
 }
 
 function calculateEntry(entrants) {
-
+  if (!Array.isArray(entrants)) return 0;
+  const { adult, senior, child } = prices;
+  const visitantes = countEntrants(entrants);
+  const { adult: adulto, child: crianca, senior: idoso } = visitantes;
+  const calculo = (adult * adulto) + (child * crianca) + (senior * idoso);
+  return calculo;
 }
-
 module.exports = { calculateEntry, countEntrants };
+// ajuda da sala de estudos
