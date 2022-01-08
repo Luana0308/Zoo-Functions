afae6cb52431fecbe8a458e9f8c5950bc0f0948e
Luana <luanaff-2008@hotmail.com>
2022-01-08 11:07:11 -0300

    requisito 9 refatorado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,18 +3,23 @@ const data = require('../data/zoo_data');
 function getOldestFromFirstSpecies(id) {
   const { employees } = data;
   const { species } = data;
-  const funcionario = employees.find((pessoa) => pessoa.id === id).responsibleFor[0];
-  const especie = species.find((animal) => animal.id === funcionario).residents;
-  const animalVelho = especie.reduce((acc, nome) => {
-    if (acc.age > nome.age) {
-      return acc;
-    }
-    return nome;
-  },
-  {});
-  const { age, name, sex } = animalVelho;
-  const resultado = [name, sex, age];
-  return resultado;
+  const idAnimal = employees.find((pessoa) => pessoa.id === id).responsibleFor[0];
+  const especies = species.find((animal) => animal.id === idAnimal).residents;
+  const ordenado = especies.sort((a, b) => b.age - a.age);
+  const olderAnimal = ordenado[0];
+  const resultadoesperado = Object.keys(olderAnimal).map((key) => olderAnimal[key]);
+  return resultadoesperado;
 }
-
+getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 module.exports = getOldestFromFirstSpecies;
+
+// const animalMaisVelho = especies.reduce((acc, nome) => {
+//   if (acc.age > nome.age) {
+//     return acc;
+//   }
+//   return nome;
+// },
+// {});
+// const { age, name, sex } = animalMaisVelho;
+// const resultado = [name, sex, age];
+// return resultado;
