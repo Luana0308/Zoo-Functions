a889c217bc15ae7686f17c7d84d7e31deee913a5
Luana <luanaff-2008@hotmail.com>
2022-01-10 22:44:01 -0300

    inicio do requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function verificaInformaçõesInválidas(parametro) {
+  const nomeFuncionarioExisteFuncionarios = employees.some((item) =>
+    parametro.name === item.firstName);
+  const sobrenomeFuncionarioExisteFuncionarios = employees.some((item) =>
+    parametro.name === item.lastName);
+  const idFuncionarioExisteFuncionarios = employees.some((item) =>
+    parametro.id === item.id);
+
+  if (!nomeFuncionarioExisteFuncionarios
+    && !sobrenomeFuncionarioExisteFuncionarios
+    && !idFuncionarioExisteFuncionarios) {
+    throw new Error('Informações inválidas');
+  }
 }
 
+function getEmployeesCoverage(parametro) {
+  verificaInformaçõesInválidas(parametro);
+}
 module.exports = getEmployeesCoverage;
